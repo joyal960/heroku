dad40d71c92ce92d57c3eccc24ed410fe205eadb
joyal <joyal@taski.in>
2022-11-04 17:32:46 +0530

    add express

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-'use strict';
-console.log("hello world");
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}) 
+app.listen(process.env.PORT ||3000,()=>{
+    console.log("serer is running");
+})
